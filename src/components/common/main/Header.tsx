db037c349c98a78326d0336f2e0e9771775b9015
wynter22 <dev.u0lee@gmail.com>
2022-03-29 02:12:51 +0900

    레이아웃 수정

--- src/components/common/main/Header.tsx
+++ src/components/common/main/Header.tsx
@@ -8,9 +8,23 @@ const Header = () => {
   return (
     <>
       <header className="">
-        {menuList.map(menu => (
-          <div key={menu.id}>{menu.name}</div>
-        ))}
+        <div className="topbar">
+          <div className="topbar__content">
+            <div>Logo</div>
+            <nav>
+              <a href="/">menu1</a>
+              <a href="/">menu2</a>
+              <a href="/">menu3</a>
+            </nav>
+          </div>
+        </div>
+        <div className="navigator">
+          <div className="navigator__content">
+            {menuList.map(menu => {
+              return <div key={menu.id}>{menu.name}</div>;
+            })}
+          </div>
+        </div>
       </header>
     </>
   );
