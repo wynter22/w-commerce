f8a875bba358d2622f54562babda43befcd3e5b1
wynter22 <dev.u0lee@gmail.com>
2022-04-13 15:02:03 +0900

    상품 디테일 초안 작성

--- src/pages/Deatil.tsx
+++ src/pages/Deatil.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import 'styles/commerce/detail.scss';
 
 const Detail = (props: any) => {
   const { productId } = useParams();
+
   return (
     <>
-      <div>
-        <h3>detail</h3>
-        <span>{productId}</span>
+      <div className="content_wrap">
+        <div>
+          <div>
+            <span>{`navigator > navigator > navigator`}</span>
+          </div>
+          <img
+            src="http://via.placeholder.com/600x600"
+            width={600}
+            height={600}
+          />
+          <div>
+            <div>partner</div>
+            <div>상품명</div>
+            <div>리뷰 평점</div>
+            <div>가격</div>
+            <div>혜택</div>
+            <div>배송</div>
+          </div>
+        </div>
       </div>
     </>
   );
